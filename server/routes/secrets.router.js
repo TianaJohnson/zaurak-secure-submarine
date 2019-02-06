741e8de4454c772cfc63ca59803a53c28293a4b3
Tiana Johnson <tiana.marie013@gmail.com>
2019-02-06 14:08:16 -0600

    authentification working

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -2,14 +2,30 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+// router.get('/', (req, res) => {
+//     console.log('req.user:', req.user);
+//     pool.query('SELECT * FROM "secret";')
+//         .then(results => res.send(results.rows))
+//         .catch(error => {
+//             console.log('Error making SELECT for secrets:', error);
+//             res.sendStatus(500);
+//         });
+// });
+
 router.get('/', (req, res) => {
-    console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
-        .then(results => res.send(results.rows))
-        .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
-            res.sendStatus(500);
-        });
-});
+    if (req.isAuthenticated()) {
+        console.log('req.user:', req.user);
+        pool.query(`SELECT * FROM "secret"
+        WHERE "secrecy_level" = $1;`,[])
+            .then(results => res.send(results.rows))
+            .catch(error => {
+                console.log('Error making SELECT for secrets:', error);
+                res.sendStatus(500);
+            });
+    } else {
+      // They are not authenticated.
+      res.sendStatus(403);
+    }
+  });
 
 module.exports = router;
